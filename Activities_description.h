a16e4ded2c9321d6a5262fb86a8dc20f505b1e20
jieke <jackylin432@gmail.com>
2024-03-19 17:39:07 +0100

    PASS RESPONSIBILITY ON TIME FROM CLASS Activity_description TO CLASS Time AND MODIFIED Activity_description CONSTRUCTOR THAT WILL THROW AN EXCEPTION WITH INVALID NAME

--- Activities_description.h
+++ Activities_description.h
@@ -2,6 +2,7 @@
 // Created by jieke on 21/02/24.
 //
 #include <iostream>
+#include "Time.h"
 #ifndef ACTIVITIES_TRACKER_ACTIVITIES_DESCRIPTION_H
 #define ACTIVITIES_TRACKER_ACTIVITIES_DESCRIPTION_H
 
@@ -9,33 +10,24 @@
 class Activities_description {
 private:
     std::string activity_name;
+    Time time;
 
 public:
-    Activities_description(std::string name, const std::string& time_start, const std::string& time_end) : activity_name(name),
-    activity_time_start(time_start), activity_time_end(time_end) {};
+
+    Activities_description(const std::string time_start, const std::string time_end, const std::string name = "" ) : activity_name(name), time(time_start, time_end) {
+        if(name == "")
+            throw std::logic_error("Activity invalid name");
+    }
+
+
 
     const std::string& get_activity_name() const{
         return activity_name;
     }
 
-    const std::string& get_activity_time_start() const{
-        return activity_time_start;
-    }
-
-    const std::string& get_activity_time_end() const{
-        return activity_time_end;
-    }
 
     void set_activity_name (const std::string& name) {
         activity_name = name;
-    }
-
-    void set_activity_time_start (const std::string& time_start) {
-        activity_time_start = time_start;
-    }
-
-    void set_activity_time_end (const std::string& time_end) {
-        activity_time_end = time_end;
     }
 };
 
