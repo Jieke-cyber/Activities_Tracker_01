dc15ef5b7461d847146653c1ab0b40fb53b7e022
jieke <jackylin432@gmail.com>
2024-03-19 17:15:10 +0100

    ADD SET AND GET METHODS FOR CLASS TIME

--- Time.h
+++ Time.h
@@ -15,7 +15,24 @@ public:
         if(time_convertor(ts)>= time_convertor(te))
             throw std::logic_error("Start time must be less than end time");
     }
+
     int time_convertor(const std::string& time) const;
+
+    const std::string& get_time_start() const {
+        return activity_time_start;
+    }
+
+    const std::string& get_time_end() const {
+        return activity_time_end;
+    }
+
+    void set_time_start(const std::string& ts ) {
+        activity_time_start = ts;
+    }
+
+    void set_time_end(const std::string& te ) {
+        activity_time_end = te;
+    }
 };
 
 
